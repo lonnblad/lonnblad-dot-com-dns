50ac6bb2d64887319fe39e10c0b031c0290ddfbb
Fredrik Lönnblad <fredrik+github@lonnblad.com>
2021-07-13 09:14:50 +0200

    Added an MX record-set for lonnblad.com

--- hosted_zone.tf
+++ hosted_zone.tf
@@ -13,6 +13,7 @@ resource "google_dns_managed_zone" "root" {
       key_type   = "keySigning"
       kind       = "dns#dnsKeySpec"
     }
+
     default_key_specs {
       algorithm  = "rsasha256"
       key_length = 1024
@@ -20,6 +21,21 @@ resource "google_dns_managed_zone" "root" {
       kind       = "dns#dnsKeySpec"
     }
   }
+}
+
+resource "google_dns_record_set" "email" {
+  managed_zone = google_dns_managed_zone.root.name
+  name         = "lonnblad.com."
+  type         = "MX"
+  ttl          = 300
+
+  rrdatas      = [
+    "1 aspmx.l.google.com.",
+    "5 alt1.aspmx.l.google.com.",
+    "5 alt2.aspmx.l.google.com.",
+    "10 alt3.aspmx.l.google.com.",
+    "10 alt4.aspmx.l.google.com."
+  ]
 }
 
 resource "google_dns_managed_zone" "projects" {
@@ -37,6 +53,7 @@ resource "google_dns_managed_zone" "projects" {
       key_type   = "keySigning"
       kind       = "dns#dnsKeySpec"
     }
+
     default_key_specs {
       algorithm  = "rsasha256"
       key_length = 1024
